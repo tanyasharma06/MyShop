22e0315b18eeddab9d83087b490c117420b5a057
tanyasharma06 <143258052+tanyasharma06@users.noreply.github.com>
2025-08-02 22:54:19 +0530

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,16 +1,9 @@
 const API_CONFIG = {
   // Development
-<<<<<<< HEAD
   // BASE_URL: 'http://localhost:5000',
-=======
-  BASE_URL: 'https://myshop-cwax.onrender.com',
->>>>>>> 5a94ade0acdf93e1ab657eb98a2b18c6bf7d7443
   
   // Production (update with your actual Render backend URL after deployment)
   BASE_URL: 'https://myshop-cwax.onrender.com'
 };
 
-<<<<<<< HEAD
-=======
 export default API_CONFIG;
->>>>>>> 5a94ade0acdf93e1ab657eb98a2b18c6bf7d7443
