fb1e6d0a6470503e74b869f73620038a7e06298b
Tanya <tanyasharma4979@gmail.com>
2025-08-02 22:56:39 +0530

    Fix import path for API_CONFIG in admin products slice

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import API_CONFIG from "../../config/api";
+import { API_CONFIG } from "../../config/api";
 
 const initialState = {
   isLoading: false,
