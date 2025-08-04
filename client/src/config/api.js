ac277aa747b852b5b1da92e592bcba291f0ba8d9
tanyasharma06 <143258052+tanyasharma06@users.noreply.github.com>
2025-08-04 14:54:26 +0530

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,18 +1,6 @@
 const API_CONFIG = {
-<<<<<<< HEAD
-  // Backend live URL
   BASE_URL: 'https://myshop-backend-fdzz.onrender.com',
-=======
-  // Development
-  BASE_URL: 'https://myshop-backend-fdzz.onrender.com',
-  
-  // Production (update with your actual Render backend URL after deployment)
-<<<<<<< HEAD
-  //BASE_URL: 'https://myshop-cwax.onrender.com'
-=======
-  BASE_URL: 'https://myshopb.onrender.com'
->>>>>>> e0c8035390464f4ab7b8f2cdb114ea8ec60fd2db
->>>>>>> 59eec1d0f426d41d022c87bae05821f8fd341dcb
+
 };
 
 export default API_CONFIG;
