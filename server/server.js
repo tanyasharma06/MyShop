8386807aab6b321a173ecda2e216487c119678b9
tanyasharma06 <143258052+tanyasharma06@users.noreply.github.com>
2025-08-04 15:27:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
     'http://localhost:5173',
-   'https://my-shop-two-rho.vercel.app'
+   'https://my-shop-leja.vercel.app'
 ];
 
 app.use(
