1c4228e8b969c2f60cab614751e8b77491ee004a
tanyasharma06 <143258052+tanyasharma06@users.noreply.github.com>
2025-08-04 14:59:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -35,7 +35,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
-    'http://localhost:5173'];
+    'http://localhost:5173',
+   'https://my-shop-two-rho.vercel.app'
+];
 
 app.use(
     cors({
